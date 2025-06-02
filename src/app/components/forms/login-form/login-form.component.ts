0928cc4be9e1a6c04cf1efd5368437c50f222db4
Bryan Morales <144483679+JalkemMorales@users.noreply.github.com>
2025-06-01 22:50:36 -0600

    Actualizacion de estilos para formularios

--- src/app/components/forms/login-form/login-form.component.ts
+++ src/app/components/forms/login-form/login-form.component.ts
@@ -2,9 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login-form',
-  standalone: false,
   templateUrl: './login-form.component.html',
-  styleUrl: './login-form.component.css'
+  styleUrls: ['./login-form.component.scss']
 })
 export class LoginFormComponent {
 
