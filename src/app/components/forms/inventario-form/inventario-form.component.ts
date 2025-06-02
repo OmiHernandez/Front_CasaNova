6a869c6ee6c542eb1f44f461a2a036dc750aba61
Bryan Morales <144483679+JalkemMorales@users.noreply.github.com>
2025-06-01 22:28:35 -0600

    Creacion de componentes para formularios

---
+++ src/app/components/forms/inventario-form/inventario-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inventario-form',
+  standalone: false,
+  templateUrl: './inventario-form.component.html',
+  styleUrl: './inventario-form.component.css'
+})
+export class InventarioFormComponent {
+
+}
