23f5b9a7fd5afbd7d25e4a3168f314e2b49bfe98
OmiHernandez <hernandez.romo.n14@gmail.com>
2025-06-01 22:29:33 -0600

    Funcionalidad, conexion con el servicio de usuarios

--- src/app/components/usuarios/lista-usuarios/lista-usuarios.component.ts
+++ src/app/components/usuarios/lista-usuarios/lista-usuarios.component.ts
@@ -8,4 +8,45 @@ import { Component } from '@angular/core';
 })
 export class ListaUsuariosComponent {
 
+  listaUsuarios:any[]=[];
+  constructor( private userService:UserServiceService){
+
+    this.userService.getUsers().subscribe((data:any)=>{
+      this.listaUsuarios = data;
+    },error => {
+      console.log("No se encontraron los usuarios");
+    })
+  }
+
+  
+
+  deleteUser(id:string){
+    console.log(id,"ID");-
+    Swal.fire({
+      title: "Estas seguro?",
+      text: "Desea eliminar el usuario permanentemente",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Eliminar"
+    }).then((result) => {
+      if (result.isConfirmed) {
+
+        this.userService.deleteUser(id).subscribe(data=>{
+          
+        },error=>{
+
+        })
+
+        Swal.fire({
+          title: "Hecho",
+          text: "El usuario ha sido eliminado",
+          icon: "success"
+        });
+        
+      }
+    });
+  }
+
 }
